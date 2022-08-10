ef1ace0bfcdec850a4c4d7f3387ef6e92b77de17
nome6 <96007386+nome6@users.noreply.github.com>
2022-08-10 21:11:33 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,11 @@
+let suggestions = [
+    "Sanskrit Notes",
+    "Physics Notes",
+    "Enlish Notes",
+    "Maths 1B Notes",
+    "Maths 1A Notes",
+    "Chemistry Notes",
+    "",
+    "",
+  
+];
